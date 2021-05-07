a7fd7ac5864ed33e4b36bf3d257f0c6634186559
Cristiano Clemente <cristiano.clemente.2000@gmail.com>
2021-05-07 19:00:48 +0100

    homepage is kinda ok

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import logo from '../images/logo.png';
+import logo from '../images/neiist_logo.png';
 
 
 const NavBar = () =>
     <Navbar bg="dark" variant="dark" expand="sm">
         <Navbar.Brand href="/" >
-            <img src={logo} width="40" height="40" className="d-inline-block align-top" alt="GCE logo" />
+            <img src={logo} height="40" className="d-inline-block align-top" alt="GCE logo" />
         </Navbar.Brand >
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
             <Nav>
-                <Nav.Link href="/sobre">Sobre</Nav.Link>
-                <Nav.Link href="/hash-code">Hash Code</Nav.Link>
-                <Nav.Link href="fixme">Thesis Master</Nav.Link>
-                <Nav.Link href="/blog">Blog</Nav.Link>
+                <Nav.Link href="/atividades">Atividades</Nav.Link>
+                <Nav.Link href="/quemsomos">Quem Somos</Nav.Link>
+                <Nav.Link href="/curso">Curso</Nav.Link>
+                <Nav.Link href="/seccoes">Seccoes</Nav.Link>
+                <Nav.Link href="/socios">Socios</Nav.Link>
+                <Nav.Link href="/estatutos">Estatutos</Nav.Link>
+                <Nav.Link href="/contactos">Contactos</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
