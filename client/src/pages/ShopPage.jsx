adc2ff22cd04da9ba54e7bb7c4079a21b556690e
Diogo Santos <diogosantoss360@gmail.com>
2022-02-25 00:51:20 +0000

    Added ProductModal and Changed ProductCard

--- client/src/pages/ShopPage.jsx
+++ client/src/pages/ShopPage.jsx
@@ -1,45 +1,129 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-import Card from 'react-bootstrap/Card';
+import { Card, Modal, FormSelect, Row, Col, Button } from 'react-bootstrap';
 
+// Not sure if needed, couldn't display image without this
+// Tried using src={require(imagePath)} but got error about not finding module
+function importAll(r) {
+    let images = {};
+    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+const images = importAll(require.context('../images/products', false, /\.(png)$/));
 
-/*
-DB Table ideas
+const ProductCard = ({ product, sizes }) => {
 
-Product
-    id
-    name
-    price
-    pathImage
-    idStock
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-Stock
-    id
-    xs
-    s
-    m
-    l
-    xl
-    xxl
----------------
-Product
-    id serial PRIMARY_KEY
-    type varchar(100)
-    size varchar(20)
-    price float
-    stock integer
-*/
+    const imagePath = product.name.replace(/\s+/g, '').concat(".png");
 
-// Idea: show cards and onclick show modal to view more photos and buy
-const ShopCard = (product) => (
-    <Card key={keyValue} style={{ width: '20rem', margin: "1em" }}>
-        <Card.Img variant="top" />
-        <Card.Body>
-            <Card.Title>{product.name}</Card.Title>
-        </Card.Body>
-    </Card>
-)
+    return (
+        <>
+            <Card style={{ width: '20rem', margin: "1em" }} onClick={handleShow}>
+                <Card.Img variant="top" src={images[imagePath].default} width={"300em"} height={"300em"}/>
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                </Card.Body>
+            </Card>
+            <ProductModal
+                product={product}
+                sizes={sizes}
+                image={imagePath}
+                show={show}
+                handleClose={handleClose}
+            />
+        </>
+    );
+}
+
+const ProductModal = ({ product, sizes, image, show, handleClose }) => {
+
+    let dropdown = null;
+    const [price, setPrice] = useState(product.price);
+
+    if (sizes.length > 0) {
+        dropdown =
+            <FormSelect aria-label="Tamanho" onChange={(value) => setPrice(value)}>
+                {sizes.map((size) => <option key={size.id} value={size.size}>{size.size}</option>)}
+            </FormSelect>
+    }
+
+    return (
+        <Modal size="lg" show={show} centered>
+            <Modal.Header closeButton onClick={handleClose}>
+                <Modal.Title>{product.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row>
+                    <Col>
+                        <img src={images[image].default} className="img-fluid" />
+                    </Col>
+                    <Col>
+                        {dropdown}
+                    </Col>
+                    <Col>
+                        <h4>Preço</h4>
+                        <p>{product.price}</p>
+                        <h4>Quantidade</h4>
+                        <input></input>
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button>Order</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+const createProduct = () => {
+    let product1 = {
+        name: "Sweat Azul",
+        size: "L",
+        price: 15.70,
+        stock: 16
+    }
+    let product2 = {
+        name: "Sweat Azul",
+        size: "M",
+        price: 15.70,
+        stock: 10
+    }
+    let product3 = {
+        name: "Sweat Vermelha",
+        size: "M",
+        price: 15.70,
+        stock: 5
+    }
+    let product4 = {
+        name: "Sweat Beje",
+        size: "XS",
+        price: 15.70,
+        stock: 2
+    }
+    let product5 = {
+        name: "Sweat Vermelha",
+        size: "XL",
+        price: 15.70,
+        stock: 3
+    }
+    let product6 = {
+        name: "Sweat Cor-de-Rosa",
+        size: "XS",
+        price: 15.70,
+        stock: 2
+    }
+
+    axios.post('/api/products', product1);
+    axios.post('/api/products', product2);
+    axios.post('/api/products', product3);
+    axios.post('/api/products', product4);
+    axios.post('/api/products', product5);
+    axios.post('/api/products', product6);
+}
 
 const ShopPage = () => {
 
@@ -52,6 +136,7 @@ const ShopPage = () => {
             .then((res) => res.json())
             .then(
                 (productsRes) => {
+                    //createProduct();
                     setProducts(productsRes);
                     setIsLoaded(true);
                 },
@@ -61,7 +146,7 @@ const ShopPage = () => {
                 },
             );
     }, []);
-    
+
     if (!isLoaded) return <div>...</div>;
     if (error) {
         return (
@@ -72,12 +157,28 @@ const ShopPage = () => {
         );
     }
     if (products) {
+
+        // Remove 'duplicate' products with same name but different size
+        const uniqueProducts = [...new Map(
+            products.map(item => [item["name"], item])
+        ).values()];
+
         return (
             <div style={{ margin: '2rem 20vw 1rem 20vw' }}>
                 <h2 style={{ textAlign: 'center' }}>Sweats EIC</h2>
-                {products.map((product) => (
-                    <ShopCard item={product} />
-                ))}
+                <Row>
+                    {uniqueProducts.map((uniqueProduct) => (
+                        <Col>
+                            <ProductCard
+                                key={uniqueProduct.id}
+                                product={uniqueProduct}
+                                sizes={products.filter(
+                                    (product) => { if (product.name == uniqueProduct.name) return product }
+                                )}
+                            />
+                        </Col>
+                    ))}    
+                </Row>
             </div>
         );
     }
