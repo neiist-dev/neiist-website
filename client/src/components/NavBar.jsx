de62cd619524e23d6c97c93ac0f68fa3a669fb60
Nelson Trindade <trindade.nelson99@gmail.com>
2022-10-05 19:22:08 +0100

    adding Navigation Bar (web+mobile)

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,65 +4,58 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import logo from '../images/neiist_logo.png';
 import UserDataContext from '../UserDataContext';
+import { GoSignOut } from "react-icons/go";
+import { summarizeName, getMemberStatus, fenixPhoto, getStatusColor } from '../hooks/dataTreatment.jsx'
+
+import style from './css/NavBar.module.css'
+import { useEffect } from 'react';
 
 const NavBar = () => {
   const { userData, setUserData } = useContext(UserDataContext);
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar expand="lg" expanded={expanded} style={{ padding: '10px' }}>
-      <Navbar.Brand as={Link} to="/">
-        <img src={logo} height="40" alt="" />
-      </Navbar.Brand>
-      <Navbar.Toggle
-        onClick={() => setExpanded(expanded ? false : "expanded")}
-      />
-      <Navbar.Collapse>
-        <Nav style={{ marginRight: 'auto' }}
-          onClick={() => setExpanded(false)}
-        >
+    <div>
+      <div className={style.navBarContainer}>
+        <Navbar expand="md" expanded={expanded} className={style.navBar}>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} className={style.logo} alt="NEIIST LOGO" />
+          </Navbar.Brand>
+          <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")}/>
+          <Navbar.Collapse>
+            <Nav style={{ marginLeft: 'auto' }} onClick={() => setExpanded(false)}>
+              <Nav.Link className={`${style.navLink} ${style.onWeb}`} as={Link} to="/sobre_nos">
+                Sobre nós
+              </Nav.Link>
+              <Nav.Link className={`${style.navLink} ${style.onWeb}`} as={Link} to="/contactos">
+                Contactos
+              </Nav.Link>
+              <Nav.Link className={`${style.navLink} ${style.onWeb}`} as={Link} to="/estudante">
+                Estudante
+              </Nav.Link>
 
-          <Nav.Link as={Link} to="/atividades">
-            Atividades
-          </Nav.Link>
-          <Nav.Link as={Link} to="/quemsomos">
-            Quem Somos
-          </Nav.Link>
-          <Nav.Link as={Link} to="/curso">
-            Curso
-          </Nav.Link>
-          <Nav.Link as={Link} to="/seccoes">
-            Secções
-          </Nav.Link>
-          <Nav.Link as={Link} to="/estatutos">
-            Estatutos
-          </Nav.Link>
-          <Nav.Link as={Link} to="/contactos">
-            Contactos
-          </Nav.Link>
+              <ActiveTecnicoStudentNavLink as={Link} to="/socio">
+                Sócios
+              </ActiveTecnicoStudentNavLink>
 
-          <ActiveTecnicoStudentNavLink as={Link} to="/socios">
-            Sócios
-          </ActiveTecnicoStudentNavLink>
+              <ActiveLMeicStudentNavLink as={Link} to="/thesismaster">
+                Thesis Master
+              </ActiveLMeicStudentNavLink>
 
-          <ActiveLMeicStudentNavLink as={Link} to="/thesismaster">
-            Thesis Master
-          </ActiveLMeicStudentNavLink>
+              <AdminNavLink as={Link} to="/admin">
+                Admin
+              </AdminNavLink>
 
-          <AdminNavLink as={Link} to="/admin">
-            Admin
-          </AdminNavLink>
-
-          <GacNavLink as={Link} to="/mag">
-            MAG
-          </GacNavLink>
-
-        </Nav>
-        <Nav style={{ marginLeft: 'auto' }}>
-          <LoginLogout userData={userData} setUserData={setUserData} />
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+              <GacNavLink as={Link} to="/mag">
+                MAG
+              </GacNavLink>
+              <LoginLogout className={`${style.navLink}`} userData={userData} setUserData={setUserData} />
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      <div className={style.navSpace}/>
+    </div>
   );
 };
 
@@ -71,7 +64,7 @@ const ActiveTecnicoStudentNavLink = ({ as, to, children }) => {
 
   if (userData && userData.isActiveTecnicoStudent) {
     return (
-      <Nav.Link as={as} to={to}>
+      <Nav.Link className={`${style.navLink} ${style.onMobile}`} as={as} to={to}>
         {children}
       </Nav.Link>
     );
@@ -84,7 +77,7 @@ const ActiveLMeicStudentNavLink = ({ as, to, children }) => {
 
   if (userData && userData.isActiveLMeicStudent) {
     return (
-      <Nav.Link as={as} to={to}>
+      <Nav.Link className={`${style.navLink} ${style.onMobile}`} as={as} to={to}>
         {children}
       </Nav.Link>
     );
@@ -97,7 +90,7 @@ const GacNavLink = ({ as, to, children }) => {
 
   if (userData && userData.isGacMember) {
     return (
-      <Nav.Link as={as} to={to}>
+      <Nav.Link className={`${style.navLink} ${style.onMobile}`} as={as} to={to}>
         {children}
       </Nav.Link>
     );
@@ -110,7 +103,7 @@ const AdminNavLink = ({ as, to, children }) => {
 
   if (userData && userData.isAdmin) {
     return (
-      <Nav.Link as={as} to={to}>
+      <Nav.Link className={`${style.navLink} ${style.onMobile}`} as={as} to={to}>
         {children}
       </Nav.Link>
     );
@@ -119,7 +112,23 @@ const AdminNavLink = ({ as, to, children }) => {
 };
 
 const LoginLogout = ({ userData, setUserData }) => {
-  if (userData) return <Logout userData={userData} setUserData={setUserData} />;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (userData){
+      fetch(`/api/members/status/${userData.username}`)
+      .then((res) => res.json())
+      .then(
+        (fetchStatus) => {
+          let newData = userData;
+          newData.status = (fetchStatus) ? fetchStatus : "NaoSocio";
+          setData(userData);
+        }
+      );
+    }
+  }, [userData]);
+
+  if (data) return <LoggedIn userData={data} setUserData={setUserData} />;
   return <Login />;
 };
 
@@ -131,24 +140,38 @@ const Login = () => (
       + `&redirect_uri=${process.env.REACT_APP_FENIX_REDIRECT_URI}`
     }
   >
-    LOGIN
+    <div style={{width: '80px', height: '30px', justifyContent:'center', background: '#D9D9D9', borderRadius: '10px', fontFamily: 'Secular One', fontStyle: 'normal', fontWeight: 400, fontSize: '16px', lineHeight: '30px', display: 'flex', alignItems: 'center', textAlign: 'center', color: '#000000',}}>Login</div>
   </Nav.Link>
 );
 
-const Logout = ({ userData, setUserData }) => (
-  <>
-    <Navbar.Text>{userData.displayName}</Navbar.Text>
-    <Nav.Link
-      as={Link}
-      to="/"
-      onClick={() => {
-        window.sessionStorage.removeItem('accessToken');
-        setUserData(null);
-      }}
-    >
-      LOGOUT
+const Logout = ({ setUserData }) => (
+  <Nav.Link className={style.navLinkLogout} as={Link} to="/" onClick={() => {
+    const removeAcessToken = new Promise(()=> {window.sessionStorage.removeItem('accessToken')});
+    removeAcessToken.then(()=>{setUserData(null)});
+  }}>
+    <GoSignOut style={{width:'25px', alignItems: 'center', color: 'black'}}/>
+  </Nav.Link>
+);
+
+const LoggedIn = ({ userData, setUserData }) => (
+  <div className={`${style.loggedSpace} ${style.onlyWeb}`}>  
+    <Nav.Link style={{padding: '0'}} as={Link} to="/socio">
+      <div className={style.loggedSpaceClickableArea} />
     </Nav.Link>
-  </>
+    <div className={style.loggedImage}
+      style={userData && {backgroundImage: `url(${fenixPhoto(userData.username)})`}}/>
+    
+    <div className={style.loggedInfo}>
+      <div className={style.loggedName}> {summarizeName(userData.displayName)} </div>
+
+      <div className={style.logoutButton_MemberState}>
+        <Logout setUserData={setUserData}/>
+        <div style={{borderRadius: '25px', marginRight: '7px', background: getStatusColor(userData.status) ,width: '125px', height: '22px', float: 'right'}}>
+          <div className={style.memberStatus}> {getMemberStatus(userData.status)} </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default NavBar;
