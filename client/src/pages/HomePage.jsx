d33cbd4ed0d483dbe3207a502a0fda581706f06e
Nelson Trindade <trindade.nelson99@gmail.com>
2022-11-07 10:02:24 +0000

    h1, paragraph and campi images

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-
-import neiistBanner from '../images/neiist_banner.jpg';
 import alameda from '../images/alameda.jpg';
 import taguspark from '../images/taguspark.jpg';
+
+import style from './css/HomePage.module.css'
 
 import useWindowSize from '../hooks/useWindowSize';
 
@@ -11,108 +10,24 @@ const HomePage = () => {
   const windowSize = useWindowSize();
 
   return (
-    <>
-      <div style={{ margin: '2rem 20vw 1rem 20vw' }}>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              style={{
-                display: 'block',
-                width: '100%',
-                height: '70vh',
-                objectFit: 'cover',
-              }}
-              src={neiistBanner}
-              alt="neiistBanner"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              style={{
-                display: 'block',
-                width: '100%',
-                height: '70vh',
-                objectFit: 'cover',
-              }}
-              src={alameda}
-              alt="alameda"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              style={{
-                display: 'block',
-                width: '100%',
-                height: '70vh',
-                objectFit: 'cover',
-              }}
-              src={taguspark}
-              alt="taguspark"
-            />
-          </Carousel.Item>
-        </Carousel>
+    <div style={{height: '100vh'}}>
+      <div style={{overflow: 'hidden', opacity: '75%', position: 'absolute', backgroundColor: '#000', width:'50%', height: '100vh', top: '-100px', left: '0px'}}>
+        <img
+          src={alameda}
+          style={{width: '200%', height: '200%', objectFit: 'contains', objectPosition: 'calc( -375px + 50%) -125px'}}/>
       </div>
-
-      <div style={{ margin: '1rem 20vw' }}>
-        <h1 style={{ textAlign: 'center' }}>MISSÃO</h1>
-        <p>
-          O NEIIST ambiciona ser o núcleo que integra, apoia e dinamiza todos os
-          grupos, comunidades e iniciativas com impacto no percurso de estudantes
-          do IST com interesses em Tecnologias de Informação.
-        </p>
-        <p>
-          Para tal, pretende criar uma plataforma que promova a cooperação entre
-          estudantes, e entre estes e docentes, órgãos institucionais e empresas,
-          centrada na criação de oportunidades e no associativismo e valorização
-          pessoal.
-        </p>
+      <div style={{overflow: 'hidden', opacity: '75%', position: 'absolute', backgroundColor: '#adcff4', width:'50%', height: '100vh', top: '-100px', right: '0px'}}>
+        <img
+          src={taguspark}
+          style={{width: '150%', height: '100%', objectFit: 'contains', objectPosition: 'calc( -250px + 20%) 200px'}}/>
       </div>
-
-      <div style={{ margin: '1rem 20vw' }}>
-        <h1 style={{ textAlign: 'center' }}>OBJETIVOS</h1>
-        <ul>
-          <li>Organizar diversas Atividades no âmbito da informática e valorização pessoal</li>
-          <li>
-            Estimular o interesse pela informática
-            e a divulgação da mesma dentro e fora do Instituto Superior Técnico
-          </li>
-          <li>
-            Contribuir para o relacionamento nacional e internacional
-            dos estudantes de informática e de outras áreas afins
-          </li>
-          <li>
-            Estimular o associativismo e o espírito de equipa
-            dentro das licenciaturas da responsabilidade do
-            Departamento de Engenharia Informática do Instituto Superior Técnico (DEI)
-          </li>
-          <li>
-            Promover a imagem das licenciaturas, mestrados e doutoramentos
-            da responsabilidade do DEI
-          </li>
-          <li>
-            Promover o relacionamento entre
-            professores, alunos, profissionais do ramo e empresas
-          </li>
-        </ul>
+      <div style={{position: 'absolute', width: '150%', top: '-50%', left: '-25%', height: 'calc( 375px + 40% )', background: 'var(--bg-color)', borderRadius: '50% / 50% ', filter: 'blur(25px)'}}/>
+      <div style={{margin: '1em 6em 0 6em', position: 'relative', width: 'calc(100% - 12em)', height:'75vh', display: 'flex', flexWrap: 'nowrap',flexFlow: 'column nowrap', alignItems: 'center', justifyContent: 'flex-start', alignContent: 'center', flexDirection: 'column'}}>
+        <h1>Núcleo Estudantil de Informática</h1>
+        <h2>do Instituto Superior Técnico</h2>
+        <p>Um grupo de estudantes motivados para ajudar todo e qualquer aluno de Engenharia Informática e Computadores (e não só), realizando atividades no ambito da informática e apoio social.</p>
       </div>
-
-      <div style={{ margin: '1rem 20vw' }}>
-        <h1 style={{ textAlign: 'center' }}>NOVIDADES</h1>
-      </div>
-
-      <div style={{ textAlign: 'center', padding: '0rem 0rem 2rem 0rem' }}>
-        <iframe
-          title="Facebook do NEIIST"
-          src={
-            windowSize.innerWidth < 600
-              ? 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNEIIST&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId' : 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNEIIST&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId'
-          }
-          width={windowSize.innerWidth < 600 ? 300 : 500}
-          height="500"
-          style={{ border: 'none' }}
-        />
-      </div>
-    </>
+    </div>
   );
 };
 
