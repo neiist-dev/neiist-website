37274cbbef7ce1b9b509a7666f4e19e39911f5ff
nelsontr <trindade.nelson99@gmail.com>
2023-07-03 02:19:51 +0100

    adding variable of selected inputs, adding preview of selection

--- client/src/pages/AdminNewSocialOrgans.jsx
+++ client/src/pages/AdminNewSocialOrgans.jsx
@@ -1,14 +1,30 @@
-import { forwardRef, useEffect, useState } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { Group, Avatar, Text, Select, Grid } from "@mantine/core";
-
 import collabs from '../images/colaboradores/collaborators.json';
-import { fenixPhoto } from "../components/functions/dataTreatment";
+import { fenixPhoto, normalizeJob, summarizeName } from "../components/functions/dataTreatment";
+import style from "./css/AboutPage.module.css";
+import DivPersonCard from "../components/aboutPage/CollabCard";
+import { getCollabImage } from "../components/functions/collabsGeneral";
+
 
 const AdminNewSocialOrgans = () => {
   const [activeMembers, setMembers] = useState(null);
   const [nextYear, setNextYear] = useState(
     `${new Date().getFullYear()}/${new Date().getFullYear() + 1}`
   );
+
+  const initialSelectedValues = Object.keys(collabs.orgaosSociais).reduce(
+    (acc, Orgao) => {
+      acc[Orgao] = {};
+      Object.keys(collabs.orgaosSociais[Orgao]).forEach((position) => {
+        acc[Orgao][position] = null;
+      });
+      return acc;
+    },
+    {}
+  );
+
+  const [selectedValues, setSelectedValues] = useState(initialSelectedValues);
 
   useEffect(() => {
     fetch("/api/mag/active")
@@ -27,30 +43,67 @@ const AdminNewSocialOrgans = () => {
       });
   }, []);
 
+  // Function to handle the selection change in the Select component
+  const handleSelectChange = (Orgao, position, value) => {
+    setSelectedValues((prevValues) => ({
+      ...prevValues,
+      [Orgao]: {
+        ...prevValues[Orgao],
+        [position]: value,
+      },
+    }));
+  };
+
+  console.log(selectedValues)
+
   return (
-    <div style={{margin: '2rem 6em 1rem 6em'}}>
+    <div style={{ margin: '2rem 6em 1rem 6em' }}>
       <h2 style={{ textDecoration: "underline" }}>Novos Orgãos Sociais</h2>
       <h3>
         Ano Letivo: <p style={{ display: "inline" }}>{nextYear}</p>
       </h3>
-      {activeMembers && <AllOrgansDiv activeMembers={activeMembers} />}
+      {activeMembers && (
+        <AllOrgansDiv
+          activeMembers={activeMembers}
+          selectedValues={selectedValues}
+          handleSelectChange={handleSelectChange}
+        />
+      )}
+      <br />
+      <hr />
+      <h2>Preview</h2>
+      {Object.entries(selectedValues).map(
+        ([socialEntity, members], index) => (
+          <SocialEntityDiv
+            key={index}
+            socialEntity={socialEntity}
+            members={members}
+            activeMembers={activeMembers}
+          />
+        )
+      )}
     </div>
   );
 };
 
-const AllOrgansDiv = ({ activeMembers }) => (
+const AllOrgansDiv = ({ activeMembers, selectedValues, handleSelectChange }) => (
   Object.keys(collabs.orgaosSociais).map((Orgao) => (
     <div key={`${Orgao}`}>
       <br />
       <h3>{Orgao}</h3>
       <Grid grow gutter="sm">
-        <OrganDiv activeMembers={activeMembers} Orgao={Orgao} />
+        <OrganDiv
+          activeMembers={activeMembers}
+          selectedValues={selectedValues[Orgao]}
+          handleSelectChange={handleSelectChange}
+          Orgao={Orgao}
+        />
       </Grid>
     </div>
   ))
 );
 
-const OrganDiv = ({ activeMembers, Orgao }) => (
+const OrganDiv = ({ activeMembers, selectedValues, handleSelectChange, Orgao }) => (
   Object.keys(collabs.orgaosSociais[Orgao]).map((position) => (
     <Grid.Col span={3} key={`${Orgao}_${position}`}>
       <Select
@@ -63,13 +116,11 @@ const OrganDiv = ({ activeMembers, Orgao }) => (
         maxDropdownHeight={250}
         nothingFound="Não existe nenhum Sócio Eleitor com esse nome."
         filter={(value, item) =>
-          item.label
-            .toLowerCase()
-            .includes(value.toLowerCase().trim()) ||
-          item.username
-            .toLowerCase()
-            .includes(value.toLowerCase().trim())
+          item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
+          item.username.toLowerCase().includes(value.toLowerCase().trim())
         }
+        onChange={(value) => handleSelectChange(Orgao, position, value)} // Call the handleSelectChange function on selection change
+        value={selectedValues[position] || null} // Set the selected value based on the stored state
       />
     </Grid.Col>
   ))
@@ -79,7 +130,6 @@ const SelectItem = forwardRef(({ image, label, username, ...others }, ref) => (
   <div ref={ref} {...others}>
     <Group noWrap>
       <Avatar size="lg" src={image} />
-
       <div>
         <Text size="md" weight={500}>
           {label}
@@ -91,5 +141,27 @@ const SelectItem = forwardRef(({ image, label, username, ...others }, ref) => (
     </Group>
   </div>
 ));
+
+const SocialEntityDiv = ({ socialEntity, members, activeMembers }) => {
+
+  const getName = (username) => activeMembers?.filter(x => x.username == username)[0]?.label; 
+
+  return (
+    <div>
+      <br />
+      <h3>{socialEntity}</h3>
+      <div className={style.socialOrgansCard}>
+        {Object.entries(members).map(([job, username], index) => (
+          <DivPersonCard
+            key={index}
+            name={summarizeName(getName(username))}
+            job={normalizeJob(job)}
+            image={getCollabImage(summarizeName(getName(username)))}
+            />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default AdminNewSocialOrgans;
