3f2325a76ba329322af7daee2ae1002f30e6d8d1
nelsontr <trindade.nelson99@gmail.com>
2023-07-03 10:55:38 +0100

    removing unnecessary code, using ai to refactor code

--- client/src/pages/AdminNewSocialOrgans.jsx
+++ client/src/pages/AdminNewSocialOrgans.jsx
@@ -1,124 +1,86 @@
-import React, { forwardRef, useEffect, useState } from "react";
-import { Group, Avatar, Text, Select, Grid } from "@mantine/core";
-import collabs from '../images/colaboradores/collaborators.json';
-import { fenixPhoto, normalizeJob, summarizeName } from "../components/functions/dataTreatment";
-import style from "./css/AboutPage.module.css";
+import React, { forwardRef, useCallback, useEffect, useState } from "react";
+import { Group, Avatar, Text, Select } from "@mantine/core";
+import produce from "immer";
+
+import collabs from "../images/colaboradores/collaborators.json";
+import {
+  fenixPhoto,
+  normalizeJob,
+  summarizeName,
+} from "../components/functions/dataTreatment";
 import DivPersonCard from "../components/aboutPage/CollabCard";
 import { getCollabImage } from "../components/functions/collabsGeneral";
 
+import style from "./css/AboutPage.module.css";
 
 const AdminNewSocialOrgans = () => {
   const [activeMembers, setMembers] = useState(null);
-  const [nextYear, setNextYear] = useState(
-    `${new Date().getFullYear()}/${new Date().getFullYear() + 1}`
-  );
+  const nextYear = `${new Date().getFullYear()}/${new Date().getFullYear() + 1}`;
 
-  const initialSelectedValues = Object.keys(collabs.orgaosSociais).reduce(
-    (acc, Orgao) => {
+  const [selectedValues, setSelectedValues] = useState(
+    Object.keys(collabs.orgaosSociais).reduce((acc, Orgao) => {
       acc[Orgao] = {};
       Object.keys(collabs.orgaosSociais[Orgao]).forEach((position) => {
         acc[Orgao][position] = null;
       });
       return acc;
-    },
-    {}
+    }, {})
   );
 
-  const [selectedValues, setSelectedValues] = useState(initialSelectedValues);
-
-  useEffect(() => {
-    fetch("/api/mag/active")
-      .then((res) => res.json())
-      .then((membersRes) => {
-        const y = membersRes?.map((person) => ({
-          image: fenixPhoto(person.username),
-          label: person.name,
-          value: person.username,
-          username: person.username,
-        }));
-        setMembers(y);
+  const handleSelectChange = useCallback((Orgao, position, value) => {
+    setSelectedValues(
+      produce((draft) => {
+        draft[Orgao][position] = value;
       })
-      .catch((err) => {
-        console.log(err);
-      });
+    );
   }, []);
-
-  // Function to handle the selection change in the Select component
-  const handleSelectChange = (Orgao, position, value) => {
-    setSelectedValues((prevValues) => ({
-      ...prevValues,
-      [Orgao]: {
-        ...prevValues[Orgao],
-        [position]: value,
-      },
-    }));
-  };
-
-  console.log(selectedValues)
-
+  
+  const fetchActiveMembers = useCallback(async () => {
+    try {
+      const res = await fetch("/api/mag/active");
+      const membersRes = await res.json();
+      const y = membersRes?.map((person) => ({
+        image: fenixPhoto(person.username),
+        label: person.name,
+        value: person.username,
+        username: person.username,
+      }));
+      setMembers(y);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+  
+    useEffect(() => {
+      fetchActiveMembers();
+    }, [fetchActiveMembers]);
+  
   return (
-    <div style={{ margin: '2rem 6em 1rem 6em' }}>
-      <div style={{display: 'flex', alignItems: 'center'}}>
-        <h2 style={{ textDecoration: "underline", width: '70%' }}>Novos Orgãos Sociais</h2>
-        <h3 style={{width: '30%'}}>
+    <div style={{ margin: "2rem 6em 1rem 6em" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h2 style={{ textDecoration: "underline", width: "70%" }}>
+          Novos Orgãos Sociais
+        </h2>
+        <h3 style={{ width: "30%" }}>
           Ano Letivo: <p style={{ display: "inline" }}>{nextYear}</p>
         </h3>
       </div>
-      {activeMembers && Object.entries(selectedValues)?.map(
-        ([socialEntity, members], index) => (
-          <SocialEntityDiv
-            key={index}
-            socialEntity={socialEntity}
-            members={members}
-            activeMembers={activeMembers}
-            selectedValues={selectedValues}
-            handleSelectChange={handleSelectChange}
-          />
-        )
-      )}
+      {activeMembers &&
+        Object.entries(selectedValues)?.map(
+          ([socialEntity, members], index) => (
+            <SocialEntityDiv
+              key={index}
+              socialEntity={socialEntity}
+              members={members}
+              activeMembers={activeMembers}
+              selectedValues={selectedValues}
+              handleSelectChange={handleSelectChange}
+            />
+          )
+        )}
     </div>
   );
 };
-
-const AllOrgansDiv = ({ activeMembers, selectedValues, handleSelectChange }) => (
-  Object.keys(collabs.orgaosSociais).map((Orgao) => (
-    <div key={`${Orgao}`}>
-      <br />
-      <h3>{Orgao}</h3>
-      <Grid grow gutter="sm">
-        <OrganDiv
-          activeMembers={activeMembers}
-          selectedValues={selectedValues[Orgao]}
-          handleSelectChange={handleSelectChange}
-          Orgao={Orgao}
-        />
-      </Grid>
-    </div>
-  ))
-);
-
-const OrganDiv = ({ activeMembers, selectedValues, handleSelectChange, Orgao }) => (
-  Object.keys(collabs.orgaosSociais[Orgao]).map((position) => (
-    <Grid.Col span={3} key={`${Orgao}_${position}`}>
-      <Select
-        label={position.replace(/[0-9]/g, "")}
-        placeholder="Escolhe um..."
-        itemComponent={SelectItem}
-        data={activeMembers}
-        searchable
-        dropdownPosition="bottom"
-        maxDropdownHeight={250}
-        nothingFound="Não existe nenhum Sócio Eleitor com esse nome."
-        filter={(value, item) =>
-          item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
-          item.username.toLowerCase().includes(value.toLowerCase().trim())
-        }
-        onChange={(value) => handleSelectChange(Orgao, position, value)} // Call the handleSelectChange function on selection change
-        value={selectedValues[position] || null} // Set the selected value based on the stored state
-      />
-    </Grid.Col>
-  ))
-);
 
 const SelectItem = forwardRef(({ image, label, username, ...others }, ref) => (
   <div ref={ref} {...others}>
@@ -136,9 +98,15 @@ const SelectItem = forwardRef(({ image, label, username, ...others }, ref) => (
   </div>
 ));
 
-const SocialEntityDiv = ({ socialEntity, members, activeMembers, selectedValues, handleSelectChange }) => {
-
-  const getName = (username) => activeMembers?.filter(x => x.username == username)[0]?.label; 
+const SocialEntityDiv = ({
+  socialEntity,
+  members,
+  activeMembers,
+  selectedValues,
+  handleSelectChange,
+}) => {
+  const getName = (username) =>
+    activeMembers?.filter((x) => x.username == username)[0]?.label;
 
   return (
     <div>
@@ -146,31 +114,37 @@ const SocialEntityDiv = ({ socialEntity, members, activeMembers, selectedValues,
       <h3>{socialEntity}</h3>
       <div className={style.socialOrgansCard}>
         {Object.entries(members).map(([job, username], index) => (
-          <div style={{display:'flex', flexDirection:'column', alignItems: 'center'}}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
             <DivPersonCard
               key={index}
               name={summarizeName(getName(username))}
               job={normalizeJob(job)}
               image={getCollabImage(summarizeName(getName(username)))}
-              />
-              <Select
-                label={normalizeJob(job)}
-                placeholder="Escolhe um..."
-                itemComponent={SelectItem}
-                data={activeMembers}
-                searchable
-                dropdownPosition="bottom"
-                maxDropdownHeight={250}
-                nothingFound="Não existe nenhum Sócio Eleitor com esse nome."
-                filter={(value, item) =>
-                  item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
-                  item.username.toLowerCase().includes(value.toLowerCase().trim())
-                }
-                onChange={(value) => handleSelectChange(socialEntity, job, value)} // Call the handleSelectChange function on selection change
-                value={selectedValues[socialEntity][job] || null} // Set the selected value based on the stored state
-              />
+            />
+            <Select
+              label={normalizeJob(job)}
+              placeholder="Escolhe um..."
+              itemComponent={SelectItem}
+              data={activeMembers}
+              searchable
+              dropdownPosition="bottom"
+              maxDropdownHeight={250}
+              nothingFound="Não existe nenhum Sócio Eleitor com esse nome."
+              filter={(value, item) =>
+                item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
+                item.username.toLowerCase().includes(value.toLowerCase().trim())
+              }
+              onChange={(value) => handleSelectChange(socialEntity, job, value)}
+              value={selectedValues[socialEntity][job] || null}
+            />
           </div>
-))}
+        ))}
       </div>
     </div>
   );
