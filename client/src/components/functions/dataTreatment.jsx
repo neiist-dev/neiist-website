26fb12936e0a4133ed87adbccdf3598b89732e4b
nelsontr <trindade.nelson99@gmail.com>
2023-07-03 02:18:42 +0100

    fixing small bug

--- client/src/components/functions/dataTreatment.jsx
+++ client/src/components/functions/dataTreatment.jsx
@@ -2,7 +2,7 @@ export const fenixPhoto = (username) =>
 `https://fenix.tecnico.ulisboa.pt/user/photo/${username}?s=10000`;
 
 export const summarizeName = (name) => {
-  if (name!==null){
+  if (name!==null && name!==undefined){
     const names = name.split(" ");
     return names[0] + " " + names[names.length - 1];
   }
