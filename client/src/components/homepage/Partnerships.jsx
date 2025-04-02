3d07c7d18e3a9cfc97ad3f02e12babda5d668492
Inês Costa <94355111+inesiscosta@users.noreply.github.com>
2025-04-03 00:47:39 +0100

    refactor(homepage): add partnerItem.jsx to simplify Partnership.jsx logic

--- client/src/components/homepage/Partnerships.jsx
+++ client/src/components/homepage/Partnerships.jsx
@@ -1,17 +1,17 @@
 import BlueWhiteBox from "./BlueWhiteBox";
-import { Row, Col } from "react-bootstrap";
+import PartnerItem from './PartnerItem.jsx';
+import { Row, _ } from "react-bootstrap";
+import style from "../../pages/css/HomePage.module.css";
 
-// Monetary
+// Monetary Partners
 import auren from "../../images/partnerships/monetary/auren.png";
 import managementSolutions from "../../images/partnerships/monetary/management_solutions.png";
 import basedInLisbon from "../../images/partnerships/monetary/based_in_lisbon.png";
 
-// Non-Monetary
+// Non-Monetary Partners
 import lage2 from "../../images/partnerships/non-monetary/lage2.png";
 import aiesec from "../../images/partnerships/non-monetary/aiesec.png";
 import magma from "../../images/partnerships/non-monetary/magma.png";
-
-import style from "../../pages/css/HomePage.module.css";
 
 const Partnerships = () => {
     return (
@@ -20,40 +20,16 @@ const Partnerships = () => {
             <BlueWhiteBox>
                 <h5 style={{ fontWeight: "bold" }}>Parcerias Monetárias</h5>
                 <Row className={style.partners} style={{ gap: "10px" }}>
-                    <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <a href="https://auren.com/pt/" target="_blank" rel="noopener noreferrer">
-                            <img src={auren} style={{ width: "50%", scale: "1.25", fontWeight: "bold" }} alt="Auren" />
-                        </a>
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <a href="https://www.managementsolutions.com/" target="_blank" rel="noopener noreferrer">
-                            <img src={managementSolutions} style={{ width: "50%", scale: "1.0", fontWeight: "bold" }} alt="Management Solutions" />
-                        </a>
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <a href="https://www.basedinlisbon.xyz/" target="_blank" rel="noopener noreferrer">
-                            <img src={basedInLisbon} style={{ width: "50%", scale: "1.0", fontWeight: "bold" }} alt="Based in Lisbon" />
-                        </a>
-                    </Col>
+                    <PartnerItem alt="Auren" src={auren} href="https://auren.com/pt/" scale="1.25" />
+                    <PartnerItem alt="Management Solutions" src={managementSolutions} href="https://www.managementsolutions.com/" />
+                    <PartnerItem alt="Based in Lisbon" src={basedInLisbon} href="https://www.basedinlisbon.xyz/" />
                 </Row>
-                <br/>
+                <br />
                 <h5 style={{ fontWeight: "bold" }}>Parcerias Não Monetárias</h5>
                 <Row className={style.partners} style={{ gap: "10px" }}>
-                    <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <a href="https://lage2.ist.utl.pt/" target="_blank" rel="noopener noreferrer">
-                            <img src={lage2} style={{ width: "50%", scale: "0.75", fontWeight: "bold" }} alt="LAGE2" />
-                        </a>
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <a href="https://aiesec.org/" target="_blank" rel="noopener noreferrer">
-                            <img src={aiesec} style={{ width: "50%", scale: "1.0", fontWeight: "bold" }} alt="AIESEC" />
-                        </a>
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "center" }}>
-                        <a href="https://magmastudio.pt/" target="_blank" rel="noopener noreferrer">
-                            <img src={magma} style={{ width: "50%", scale: "1.0", fontWeight: "bold" }} alt="Magma Studio" />
-                        </a>
-                    </Col>
+                    <PartnerItem alt="LAGE2" src={lage2} href="https://lage2.ist.utl.pt/" scale="0.75" />
+                    <PartnerItem alt="AIESEC" src={aiesec} href="https://aiesec.org/" />
+                    <PartnerItem alt="Magma Studio" src={magma} href="https://magmastudio.pt/" />
                 </Row>
             </BlueWhiteBox>
         </div>
