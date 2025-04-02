e14d69019e0eb7d60a587df8c902964494f28138
João Costa <joaoluissaraivacosta@gmail.com>
2025-04-02 02:12:30 +0100

    feat: Highlight all coordinator cards.

--- client/src/components/aboutPage/CollabCard.jsx
+++ client/src/components/aboutPage/CollabCard.jsx
@@ -1,11 +1,20 @@
 import { Card } from 'react-bootstrap';
-import style from '../../pages/css/AboutPage.module.css'
+import style from '../../pages/css/AboutPage.module.css';
 
-const DivPersonCard = ({ name, job, image }) => (
-  <div className={style.cardContainer}>
-    <PersonCard name={name} job={job} src={image}/>
-  </div>
-);
+const DivPersonCard = ({ name, job, image, teams }) => {
+  console.log("teams -> ", teams);
+  const isCoordinator = teams?.includes('COOR-');
+
+  return (
+    <div className={style.cardContainer}>
+      {isCoordinator ? (
+        <CoordinatorCard name={name} job={job} src={image} />
+      ) : (
+        <PersonCard name={name} job={job} src={image} />
+      )}
+    </div>
+  );
+};
 
 const PersonCard = ({ name, job, src }) => (
   <Card className={`${style.card}`}>
@@ -13,6 +22,17 @@ const PersonCard = ({ name, job, src }) => (
     <Card.ImgOverlay className={style.cardImgOver}>
       <Card.Title bsPrefix={style.cardTitle}>{name}</Card.Title>
       {job && <Card.Text bsPrefix={style.cardText}>{job}</Card.Text>}
+    </Card.ImgOverlay>
+  </Card>
+);
+
+const CoordinatorCard = ({ name, job, src }) => (
+  <Card className={`${style.card} ${style.coordinatorCard}`}>
+    <Card.Img className={`${style.cardImg} hover-zoom hover-shadow`} variant="top" src={src} />
+    <Card.ImgOverlay className={style.cardImgOver}>
+      <Card.Title bsPrefix={style.cardTitle}>{name}</Card.Title>
+      {job && <Card.Text bsPrefix={style.cardText}>{job}</Card.Text>}
+      <div className={style.coordinatorBadge}>Coordenador(a)</div>
     </Card.ImgOverlay>
   </Card>
 );
