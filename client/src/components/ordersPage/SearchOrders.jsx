1817aecba907580ac924ccb18c0ac227a79c332d
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-05-28 00:14:52 +0100

    feat(orders): update order details modal and search orders component for improved functionality

--- client/src/components/ordersPage/SearchOrders.jsx
+++ client/src/components/ordersPage/SearchOrders.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { TextInput, Button, Group, Text } from "@mantine/core";
 import { OrdersTable } from "./OrdersTable";
 import LoadSpinner from "../../hooks/loadSpinner.jsx";
-import { fetchAllOrdersDetails } from "../../Api.service.js";
 import { BiSearch } from "react-icons/bi";
 
-export const SearchOrders = ({ keySelected, loggedInUser }) => {
+export const SearchOrders = ({ keySelected, loggedInUser, orders }) => {
   const [allOrders, setOrders] = useState(null);
   const [filteredOrders, setFilteredOrders] = useState([]);
   const [searchInput, setSearchInput] = useState("");
@@ -13,17 +12,11 @@ export const SearchOrders = ({ keySelected, loggedInUser }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     if (keySelected === "search" && allOrders === null) {
-      fetchAllOrdersDetails()
-        .then((ordersRes) => {
-          setOrders(ordersRes);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          setError(err);
-          setIsLoading(false);
-        });
+      setOrders(orders);
     }
+    setIsLoading(false);
   }, [keySelected]);
 
   const handleSearch = (value) => {
@@ -33,20 +26,20 @@ export const SearchOrders = ({ keySelected, loggedInUser }) => {
       setFilteredOrders([]);
       return;
     }
-
+    
     const searchTerm = value.toLowerCase();
     const filtered =
-      allOrders?.filter(
-        (order) =>
-          order.order_id.toLowerCase().includes(searchTerm) ||
-          order.name.toLowerCase().includes(searchTerm) ||
-          order.ist_id.toLowerCase().includes(searchTerm) ||
-          order.email.toLowerCase().includes(searchTerm) ||
-          (order.notes && order.notes.toLowerCase().includes(searchTerm)) ||
-          order.campus.toLowerCase().includes(searchTerm) ||
-          (order.nif && order.nif.toLowerCase().includes(searchTerm))
-      ) ?? [];
-
+    allOrders?.filter(
+      (order) =>
+        order.order_id.toLowerCase().includes(searchTerm) ||
+      order.name.toLowerCase().includes(searchTerm) ||
+      order.ist_id.toLowerCase().includes(searchTerm) ||
+      order.email.toLowerCase().includes(searchTerm) ||
+      (order.notes && order.notes.toLowerCase().includes(searchTerm)) ||
+      order.campus.toLowerCase().includes(searchTerm) ||
+      (order.nif && order.nif.toLowerCase().includes(searchTerm))
+    ) ?? [];
+    
     setFilteredOrders(filtered);
   };
 
