95f120758d043315d718eb4cabb9802453ee8dd4
João Costa <joaoluissaraivacosta@gmail.com>
2025-07-23 15:08:26 +0100

    feat (css): Added shadcn-ui module components.

--- src/components/blog/BlogHeader.tsx
+++ src/components/blog/BlogHeader.tsx
@@ -3,7 +3,9 @@ import styles from '@/styles/pages/BlogPage.module.css';
 
 const BlogHeader = () => (
   <div className={styles.header}>
-    <h1 className={styles.title}>Bem-vindo/a ao Blog do Núcleo!</h1>
+    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+      Bem vindo/a ao Blog do núcleo!
+    </h1>
     <div className={styles.headerInfo}>
       <span>O teu espaço para te manteres atualizado! Publicamos regularmente as últimas novidades,</span>
       <span> eventos e informações mais importantes para todos os estudantes.</span>
