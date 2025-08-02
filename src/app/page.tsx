c7546634a89fa46a65f089bdc13fef7e5928957e
Miguel Raposo <migpovrap@hotmail.com>
2025-08-02 21:27:12 +0100

    feat(Hero): new homepage hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import SINFO from "@/components/homepage/Sinfo";
 import Partnerships from "@/components/homepage/Partnerships";
 import style from "@/styles/pages/HomePage.module.css";
+import Hero from "@/components/homepage/Hero";
+import Activities from "@/components/homepage/Activities";
 
 const HomePage = () => {
   return (
     <div className={style.homepage}>
+      <Hero />
+      <Activities />
       <SINFO />
       <Partnerships />
     </div>
--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,24 +1,28 @@
 import Image from "next/image";
-import background from "@/assets/homepage/hero.png";
+import heroAlameda from "@/assets/homepage/alameda.png";
+import heroTaguspark from "@/assets/homepage/taguspark.png";
 import styles from "@/styles/components/homepage/Hero.module.css";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <div className={styles.backgroundImage}>
-        <Image src={background} alt="Background Image" />
-      </div>
-      <div className={styles.initialBlur} />
-      <div className={styles.titles}>
-        <p>Núcleo Estudantil de Informática do Instituto Superior Técnico</p>
-        <p>
-          Um grupo de estudantes motivados para ajudar todo e qualquer aluno de Engenharia
-          Informática e Computadores (e não só), realizando atividades no âmbito da informática e
-          apoio social.
+    <section className={styles.hero}>
+        <h1 className={styles.title}>
+          Núcleo Estudantil de Informática do Instituto Superior Técnico
+        </h1>
+        <p className={styles.subtitle}>
+        Um grupo de estudantes motivados para ajudar todo e qualquer aluno de Engenharia
+        Informática e <br/> Computadores (e não só), realizando atividades no
+        ambito da informática e apoio social.
         </p>
+      <div className={styles.campusImages}>
+        <div className={styles.alameda}>
+          <Image src={heroAlameda} alt="Alameda View" fill priority style={{ objectFit: "cover" }} />
+        </div>
+        <div className={styles.taguspark}>
+          <Image src={heroTaguspark} alt="IST Building" fill priority style={{ objectFit: "cover" }} />
+        </div>
       </div>
-      <div className={styles.finalBlur} />
-    </div>
+    </section>
   );
 };
 
