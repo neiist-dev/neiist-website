5655144e407507962e42f02ba7197b908ab15439
João Costa <joaoluissaraivacosta@gmail.com>
2025-10-13 15:54:48 +0100

    refactor (hero): New hero styling (todo: responsive components)

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,38 +1,20 @@
 import Image from "next/image";
-import heroAlameda from "@/assets/homepage/alameda.png";
-import heroTaguspark from "@/assets/homepage/taguspark.png";
+import heroIST from "@/assets/homepage/istHero.png";
 import styles from "@/styles/components/homepage/Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <h1 className={styles.title}>
-        Núcleo Estudantil de Informática do Instituto Superior Técnico
+        Núcleo Estudantil de
+        <span className={styles.primary}> In</span>
+        <span className={styles.secondary}>for</span>
+        <span className={styles.tertiary}>mát</span>
+        <span className={styles.quaternary}>ica </span>
+        do Instituto Superior Técnico
       </h1>
-      <p className={styles.subtitle}>
-        Um grupo de estudantes motivados para ajudar todo e qualquer aluno de Engenharia Informática
-        e <br /> Computadores (e não só), realizando atividades no ambito da informática e apoio
-        social.
-      </p>
       <div className={styles.campusImages}>
-        <div className={styles.alameda}>
-          <Image
-            src={heroAlameda}
-            alt="Alameda View"
-            fill
-            priority
-            style={{ objectFit: "cover" }}
-          />
-        </div>
-        <div className={styles.taguspark}>
-          <Image
-            src={heroTaguspark}
-            alt="IST Building"
-            fill
-            priority
-            style={{ objectFit: "cover" }}
-          />
-        </div>
+        <Image src={heroIST} alt="Campus Views" fill priority />
       </div>
     </section>
   );
