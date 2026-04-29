de3b5b819293130a75879fecfda606c1388df766
Miguel Raposo <migpovrap@hotmail.com>
2026-04-29 08:40:02 +0100

    fix(dinner): remove fullscreen from waiting page (only be used to surprise page), add link to InfoListItem

--- src/app/dinner/page.tsx
+++ src/app/dinner/page.tsx
@@ -3,13 +3,12 @@ import Image from "next/image";
 import { getAllProducts, getAllOrders } from "@/utils/dbUtils";
 import { isJantarDeCursoCategory } from "@/utils/shop/orderKindUtils";
 import { serverCheckRoles } from "@/utils/permissionUtils";
-import styles from "@/styles/pages/DinnerPage.module.css";
-import FullScreenWrapper from "@/components/FullScreenWrapper";
 import InfoListItem from "@/components/dinner/InfoListItem";
-import penguinImg from "@/assets/events/DinnerPenguin.png";
-import { FaMapMarkerAlt, FaCalendarAlt, FaClock } from "react-icons/fa";
-import localFont from "next/font/local";
 import Countdown from "@/components/dinner/Countdown";
+import { FaMapMarkerAlt, FaCalendarAlt, FaClock } from "react-icons/fa";
+import penguinImg from "@/assets/events/DinnerPenguin.png";
+import localFont from "next/font/local";
+import styles from "@/styles/pages/DinnerPage.module.css";
 
 const handelsonTwo = localFont({
   src: "../../assets/fonts/handelson-two.otf",
@@ -46,55 +45,58 @@ export default async function DinnerPage() {
     );
     if (hasJantarOrder) {
       return (
-        <FullScreenWrapper>
-          <div className={styles.container}>
-            <div className={styles.contentWrapper}>
-              <div className={styles.leftColumn}>
-                <h1 className={`${styles.mainTitle} ${handelsonTwo.className}`}>
-                  <span className={styles.jantar}>JANTAR</span>
-                  <span className={styles.de}>de</span>
-                  <span className={styles.curso}>CURSO</span>
-                </h1>
+        <div className={styles.container}>
+          <div className={styles.contentWrapper}>
+            <div className={styles.leftColumn}>
+              <h1 className={`${styles.mainTitle} ${handelsonTwo.className}`}>
+                <span className={styles.jantar}>JANTAR</span>
+                <span className={styles.de}>de</span>
+                <span className={styles.curso}>CURSO</span>
+              </h1>
 
-                <p className={`${styles.signedUpMessage} ${handelsonTwo.className}`}>
-                  O teu lugar no jantar de curso está garantido! Prepara-te, temos surpresas à tua
-                  espera.
-                </p>
+              <p className={`${styles.signedUpMessage} ${handelsonTwo.className}`}>
+                O teu lugar no jantar de curso está garantido! Prepara-te, temos surpresas à tua
+                espera.
+              </p>
 
-                <ul className={`${styles.infoList} ${handelsonTwo.className}`}>
-                  <InfoListItem icon={<FaMapMarkerAlt />} label="Local" value="MADSpot" />
-                  <InfoListItem icon={<FaCalendarAlt />} label="Data" value="21 de maio" />
-                  <InfoListItem icon={<FaClock />} label="Hora" value="20h00 - 04h00" />
-                </ul>
-
-                {!isUnlocked ? (
-                  <div className={styles.lockedSection}>
-                    <p className={`${styles.unlockTimeMessage} ${handelsonTwo.className}`}>
-                      O conteúdo será desbloqueado às{" "}
-                      <span className={styles.highlight}>20h do dia 21 de maio</span>
-                    </p>
-                    <Countdown />
-                  </div>
-                ) : (
-                  <div className={styles.unlockedSection}>
-                    <p className={`${styles.unlockMessage} ${handelsonTwo.className}`}>
-                      🎉 Já podes aceder às surpresas!
-                    </p>
-                  </div>
-                )}
-              </div>
-
-              <div className={styles.rightColumn}>
-                <Image
-                  src={penguinImg}
-                  alt="Poster do Jantar de Curso"
-                  className={styles.image}
-                  priority
+              <ul className={`${styles.infoList} ${handelsonTwo.className}`}>
+                <InfoListItem
+                  icon={<FaMapMarkerAlt />}
+                  label="Local"
+                  value="MADSpot"
+                  url="https://www.instagram.com/mad_spot_fa/"
                 />
-              </div>
+                <InfoListItem icon={<FaCalendarAlt />} label="Data" value="21 de maio" />
+                <InfoListItem icon={<FaClock />} label="Hora" value="20h00 - 04h00" />
+              </ul>
+
+              {!isUnlocked ? (
+                <div className={styles.lockedSection}>
+                  <p className={`${styles.unlockTimeMessage} ${handelsonTwo.className}`}>
+                    O conteúdo será desbloqueado às{" "}
+                    <span className={styles.highlight}>20h do dia 21 de maio</span>
+                  </p>
+                  <Countdown />
+                </div>
+              ) : (
+                <div className={styles.unlockedSection}>
+                  <p className={`${styles.unlockMessage} ${handelsonTwo.className}`}>
+                    🎉 Já podes aceder às surpresas!
+                  </p>
+                </div>
+              )}
+            </div>
+
+            <div className={styles.rightColumn}>
+              <Image
+                src={penguinImg}
+                alt="Poster do Jantar de Curso"
+                className={styles.image}
+                priority
+              />
             </div>
           </div>
-        </FullScreenWrapper>
+        </div>
       );
     }
   }
@@ -110,7 +112,12 @@ export default async function DinnerPage() {
           </h1>
 
           <ul className={`${styles.infoList} ${handelsonTwo.className}`}>
-            <InfoListItem icon={<FaMapMarkerAlt />} label="Local" value="MADSpot" />
+            <InfoListItem
+              icon={<FaMapMarkerAlt />}
+              label="Local"
+              value="MADSpot"
+              url="https://www.instagram.com/mad_spot_fa/"
+            />
             <InfoListItem icon={<FaCalendarAlt />} label="Data" value="21 de maio" />
             <InfoListItem icon={<FaClock />} label="Hora" value="20h00 - 04h00" />
           </ul>
--- src/components/dinner/InfoListItem.tsx
+++ src/components/dinner/InfoListItem.tsx
@@ -5,13 +5,23 @@ interface InfoListItemProps {
   icon: string | React.ReactNode;
   label: string;
   value: string;
+  url?: string;
 }
 
-export default function InfoListItem({ icon, label, value }: InfoListItemProps) {
+export default function InfoListItem({ icon, label, value, url }: InfoListItemProps) {
   return (
     <li className={styles.infoItem}>
       <span className={styles.icon}>{icon}</span>
-      <strong>{label}:</strong> <span className={styles.infoValue}>{value}</span>
+      <strong>{label}:</strong>{" "}
+      <span className={styles.infoValue}>
+        {url ? (
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {value}
+          </a>
+        ) : (
+          value
+        )}
+      </span>
     </li>
   );
 }
