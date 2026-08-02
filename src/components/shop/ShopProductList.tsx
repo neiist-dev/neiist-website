2c0e0c5b36454e3a6457a4b260052a7f74ef5308
Miguel Raposo <migpovrap@hotmail.com>
2026-08-02 13:09:54 +0100

    refactor: remove "use client" from pure display components

--- src/components/shop/ShopProductList.tsx
+++ src/components/shop/ShopProductList.tsx
@@ -1,4 +1,3 @@
-"use client";
 import styles from "@/styles/components/shop/ShopProductList.module.css";
 import { Product } from "@/types/shop/product";
 import { Category } from "@/types/shop/category";
